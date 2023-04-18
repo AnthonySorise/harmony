9ed4328483952ef24667ed170f1f60d42ac1c179
Anthony Sorise <contact@anthonysorise.com>
2023-04-17 20:15:44 -0400

    rotation-animation-test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { gsap } from 'gsap';
 
 function App() {
+  const [isRotatingRight, setIsRotatingRight] = useState(true);
+  const [rotationDegrees, setRotationDegrees] = useState(0);
+
+  const StyledButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.primary.contrastText,
+    backgroundColor: theme.palette.primary.main,
+    '&:hover': {
+        backgroundColor: theme.palette.primary.dark,
+    },
+}));
+
+  const onButtonClick = () => {
+    setIsRotatingRight(!isRotatingRight);
+  };
+
+  useEffect(() => {
+    const rotationTween = gsap.to('#logo', {
+        duration: 2,
+        rotation: isRotatingRight ? `+=360` : `-=360`,
+        repeat: -1,
+        ease: "none",
+        onComplete: () => {
+          setRotationDegrees(isRotatingRight ? rotationDegrees + 360 : rotationDegrees - 360);
+        },
+      });
+  
+    return () => {
+      rotationTween.kill();
+    };
+  }, [isRotatingRight, rotationDegrees]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={logo}
+          className="App-logo"
+          id="logo"
+          alt="logo"
+        />
+        <div>
+          <StyledButton onClick={onButtonClick}>Change Rotation!</StyledButton>
+        </div>
       </header>
     </div>
   );
